876c7d929e90b9748a2a7658baa78f7dd0dd0217
Mate Bence <m.bence05@gmail.com>
2018-08-05 16:32:07 +0200

    Cennik model

---
+++ application/models/Cennik_model.php
@@ -0,0 +1,32 @@
+<?php
+
+class Cennik_model extends CI_model
+{
+    public function novy_cennik($cena = array())
+    {
+        $nova_cena = $this->db->insert('cennik', $cena);
+        if ($nova_cena) {
+            return $this->db->insert_id();
+        } else {
+            return 0;
+        }
+    }
+
+    public function aktualizuj_cennik($id_cennik, $aktualizacne_udaje)
+    {
+        if (!empty($aktualizacne_udaje)) {
+            $this->db->where('idCennik', $id_cennik);
+            $this->db->update('cennik', $aktualizacne_udaje);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function odstran_cennik($id_cennik)
+    {
+        $odstran = $this->db->delete('cennik', array('idCennik' => $id_cennik));
+        return $odstran ? true : false;
+    }
+}
+?>
