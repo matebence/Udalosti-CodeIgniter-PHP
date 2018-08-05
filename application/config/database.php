3fa6457e171f81ffb95ec93bf2c000165cf0a4e1
Mate Bence <m.bence05@gmail.com>
2018-08-05 15:51:11 +0200

    Konfiguracne subory nastavene

--- application/config/database.php
+++ application/config/database.php
@@ -73,24 +73,58 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+/*
+| -------------------------------------------------------------------
+| Pouzivatel - Udalosti(Pouzivatelia)
+| -------------------------------------------------------------------
+|Praca z databazou na pouzivatelsom urovni
+*/
 $db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
+    'dsn' => '',
+    'hostname' => 'localhost',
+    'username' => 'udalosti',
+    'password' => 'udalosti2018Pouzivatelia',
+    'database' => 'udalosti',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
+
+/*
+| -------------------------------------------------------------------
+| Pouzivatel - Admin
+| -------------------------------------------------------------------
+|Praca z databazou na urovni admina
+*/
+$db['admin'] = array(
+    'dsn' => '',
+    'hostname' => 'localhost',
+    'username' => 'admin',
+    'password' => 'uDalost2018I',
+    'database' => 'udalosti',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
 );
