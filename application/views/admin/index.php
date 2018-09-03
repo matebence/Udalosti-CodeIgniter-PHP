5972a5497516b7738c355037dea971586b4b5893
Mate Bence <m.bence05@gmail.com>
2018-09-03 17:50:49 +0200

    kniznica juery pridana

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -31,16 +31,31 @@
     </head>
 
     <body>
+        <div class="prihlasovanie">
+            <div class="logo">
+                <img id="obrazok" src="<?php echo base_url() . "assets/img/"; ?>udalosti_logo.png" alt="Udalosti logo" title="Udalosti logo">
+                <h1 id="nazov">Udalosti</h1>
+            </div>
 
-        <div class="logo">
-            <img id="obrazok" src="<?php echo base_url() . "assets/img/"; ?>udalosti_logo.png" alt="Udalosti logo" title="Udalosti logo">
-            <h1 id="nazov">Udalosti</h1>
+            <div class="formular">
+                <form action="<?php echo site_url('prihlasenie/prihlasit_sa'); ?>">
+                    <input type="text" name="email" placeholder="Email" />
+                    <input type="password" name="heslo" placeholder="Heslo" />
+                    <input type="submit" name="pokus_o_prihlasenie" value="Prihlásiť sa">
+                </form>
+            </div>
+
+            <div class="zabudnuteHeslo">
+                <form action="<?php echo site_url('prihlasenie/prihlasit_sa'); ?>">
+                    <input type="text" name="email" placeholder="Email" />
+                    <input type="submit" name="pokus_o_prihlasenie" value="Poslať">
+                </form>
+            </div>
+
+            
         </div>
-
-        <form action="<?php echo site_url('prihlasenie/prihlasit_sa'); ?>">
-            <input type="text" name="email" />
-            <input type="text" name="heslo" />
-            <input type="submit" name="pokus_o_prihlasenie" value="Prihlásiť sa">
-        </form>
+        <footer class="pata">
+            <p>©2018 Udalosti</p>
+        </footer>
     </body>
 </html>
